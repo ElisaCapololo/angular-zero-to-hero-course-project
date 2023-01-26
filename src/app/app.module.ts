6acd82dd2a853517d43a67b12e62fd602a5acaf2
Elisa Capololo <elisacapololo2017@gmail.com>
2023-01-26 00:19:39 +0000

    add local data and build the movie trending

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
@@ -8,6 +9,7 @@ import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { MovieComponent } from './movie/movie.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,9 @@ import { MovieComponent } from './movie/movie.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  /**Variabel that take the trendingMovies and send a response to that */
+  trendingMovies: any;
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.getTrendingMovies();
+  }
+
+  /**Function that inject our json (data) */
+  getTrendingMovies(){
+    this.http.get('http://localhost:4200/assets/data/trending-movies.json')
+    .subscribe((movies) => {
+      this.trendingMovies = movies;
+      console.log(movies)
+    });
   }
 
 }
